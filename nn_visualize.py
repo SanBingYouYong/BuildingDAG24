3b2376cc71d5e3b8527f56a7d842dd84f4a2bf51
Shuyuan Zhang <s2047783@ed.ac.uk>
2024-03-26 14:45:39 +0000

    trainings done

--- nn_visualize.py
+++ nn_visualize.py
@@ -9,18 +9,17 @@ def visualize_loss(loss_path: str, curve_path: str=None):
     train_losses = loss_log['train_losses']
     val_losses = loss_log['val_losses']
 
-    max_len = max(len(train_losses), len(val_losses))
-    epochs = range(1, max_len + 1)
+    epochs = range(1, len(train_losses) + 1)
 
-    plt.plot(epochs[:len(train_losses)], train_losses, 'b', label='Training loss')
-    plt.plot(epochs[:len(val_losses)], val_losses, 'r', label='Validation loss')
+    plt.plot(epochs, train_losses, 'b', label='Training loss')
+    plt.plot(epochs, val_losses, 'r', label='Validation loss')
     plt.title('Training and Validation Loss')
     plt.xlabel('Iterations')
     plt.ylabel('Loss')
     plt.legend()
 
     # Set y-axis limits to +1 of second epoch loss
-    ylim_value = max(val_losses[1:] + train_losses[1:]) + 1 if max_len > 1 else max(val_losses + train_losses)
+    ylim_value = val_losses[1] + 1 if len(val_losses) > 1 else val_losses[0]
     plt.ylim(0, ylim_value)
 
     pdf_path = curve_path if curve_path else loss_path.replace('.yml', '.pdf')
@@ -28,7 +27,6 @@ def visualize_loss(loss_path: str, curve_path: str=None):
     plt.savefig(pdf_path)
 
 
-
 if __name__ == "__main__":
-    loss_path = "./models/model_DAGDataset100_100_5_20240326122805_loss.yml"
+    loss_path = "./models/model_DAGDataset100_100_5_20240326141027_loss.yml"
     visualize_loss(loss_path)
