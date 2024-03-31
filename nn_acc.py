8d79adbfb56b4669d921ccf2301526d2e46eaaea
Shuyuan Zhang <s2047783@ed.ac.uk>
2024-03-31 17:52:07 +0100

    f1 instead of acc

--- nn_acc.py
+++ nn_acc.py
@@ -1,4 +1,5 @@
 import yaml
+from sklearn.metrics import f1_score
 
 
 DISCRETE_VARIABLES = [
@@ -42,23 +43,29 @@ if __name__ == "__main__":
 
     categories = [
         "Bm Base Shape",
-        # "Rf Base Shape",
-        # "Num Floors", 
-        # "Num Windows Each Side", 
-        # "Has Floor Ledge"
+        "Rf Base Shape",
+        "Num Floors", 
+        "Num Windows Each Side", 
+        "Has Floor Ledge",
+        "Has Window Ledge",
+        "Window Divided Horizontal",
+        "Window Divided Vertical"
     ]
     for category in categories:
         print(f"Category: {category}")
-        # calculate accuracy
-        correct = 0
-        total = 0
-        for i, (pred, target) in enumerate(results[category]):
-            pred = pred
-            target = target
-            if pred == target:
-                correct += 1
-            total += 1
-        print(f"Accuracy: {100 * correct / total:.2f}%")
+        # # calculate accuracy
+        # correct = 0
+        # total = 0
+        # for i, (pred, target) in enumerate(results[category]):
+        #     pred = pred
+        #     target = target
+        #     if pred == target:
+        #         correct += 1
+        #     total += 1
+        # print(f"Accuracy: {100 * correct / total:.2f}%")
+        # F1
+        f1 = f1_score([target for _, target in results[category]], [pred for pred, _ in results[category]], average="weighted")
+        print(f"F1: {f1:.2f}")
 
 
 
