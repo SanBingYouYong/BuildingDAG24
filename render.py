3704b71cc94e2be9916e79cd4c09a1772e93522b
Shuyuan Zhang <rayzhang707@gmail.com>
2024-04-02 11:41:21 +0100

    full building edge extraction working

--- render.py
+++ render.py
@@ -2,6 +2,10 @@ import bpy
 import cv2
 import numpy as np
 
+from distortion import DRStraight, DRCylindrical, DRSphered, DRUtils, Dedicated_Renderer
+from building4distortion import building_for_distortion_render
+import bmesh
+
 
 class DAGRenderer():
     def __init__(self, 
@@ -9,7 +13,9 @@ class DAGRenderer():
                  down_angle: float=30., 
                  lr_angle: float=35.,
                  view_angle_disturb_range: float=20.,
-                 camera_track_radius: float=2.) -> None:
+                 camera_track_radius: float=2.,
+                 building_name: str="Building",
+                 node_name: str="Building For Distortion Render") -> None:
         self.temp_file_path = temp_file_path
         self.camera = bpy.data.objects["Camera"]
         self.camera_track = bpy.data.objects["CameraTrack"]
@@ -17,6 +23,8 @@ class DAGRenderer():
         self.lr_angle = lr_angle
         self.view_angle_disturb_range = view_angle_disturb_range
         self.camera_track_radius = camera_track_radius
+        self.building_name = building_name
+        self.node_name = node_name
     
     def _post_process(self, file_path: str=None):
         path = file_path if file_path else self.temp_file_path
@@ -41,15 +49,66 @@ class DAGRenderer():
         track_radius_scaler = radius / self.camera_track_radius
         self.camera_track.scale = (track_radius_scaler, track_radius_scaler, track_radius_scaler)
 
-    def render(self, file_path: str=None):
+    def render(self, file_path: str=None, distortion: bool=False):
         # update camera position
         self._update_camera()
         # render image
         path = file_path if file_path else self.temp_file_path
         bpy.context.scene.render.filepath = path
-        bpy.ops.render.render(write_still=True)
+        if not distortion:
+            bpy.ops.render.render(write_still=True)
+        else:
+            # render with distortion
+            self.render_with_distortion()
         # post process
         self._post_process(path)
+
+    def render_with_distortion(self):
+        # check if the distortion renderer node is present
+        building = bpy.data.objects[self.building_name]
+        node = None
+        for mod in building.modifiers:
+            if mod.type == "NODES" and mod.node_group.name == self.node_name:
+                node = mod
+                break
+        if not node:
+            raise ValueError(f"Distortion renderer node '{self.node_name}' not found")
+        # get visible edges on full building
+        fb_verts, fb_edges, fb_faces = self._get_visibles(building)
+        print(f"Visible edges on full building: {len(fb_edges)}")
+        
+
+    
+    def _get_visibles(self, obj, mode="EDGE", hide_mesh=True):
+        '''
+        Get visible elements from actual mesh duplicate of obj, and by default hide the generated dup mesh. 
+        '''
+        dup = self._get_actual_mesh(obj)
+        self._set_as_active(dup)
+        verts, edges, faces = DRUtils.get_visibles(dup, mode)
+        if hide_mesh:
+            dup.hide_viewport = True
+        self._set_as_active(obj)
+        return verts, edges, faces
+
+    def _set_as_active(self, obj):
+        '''
+        De-select and de-activate the current obj, then select and activate the new obj.
+        '''
+        bpy.context.object.select_set(False)
+        bpy.context.view_layer.objects.active = obj
+        bpy.context.object.select_set(True)
+
+    def _get_actual_mesh(self, building):
+        '''
+        Copy paste a building from dag node and convert the duplicate to actual mesh, then return the mesh.
+        '''
+        self._set_as_active(building)
+        bpy.ops.object.duplicate()
+        dup = bpy.context.active_object
+        bpy.ops.object.convert(target='MESH')
+        self._set_as_active(building)
+        return dup
     
     def update_lr_angle(self, lr_angle: float):
         self.lr_angle = lr_angle
@@ -130,4 +189,5 @@ class DAGRenderer():
 
 if __name__ == "__main__":
     renderer = DAGRenderer()
-    renderer.render("datasets/test_dataset/images/1.png")
+    # renderer.render("datasets/test_dataset/images/1.png")
+    renderer.render_with_distortion()
--- temp.py
+++ temp.py
@@ -1,7 +1,17 @@
 import bpy
 
+# import local modules
+import sys
+from pathlib import Path
 
-# render
-bpy.context.scene.render.image_settings.file_format = 'PNG'
-bpy.context.scene.render.filepath = 'temp.png'
-bpy.ops.render.render(write_still=True)
+file = Path(__file__).resolve()
+parent = file.parents[1]
+sys.path.append(str(parent))
+
+
+from distortion import DRUtils
+
+building = bpy.data.objects["Building"]
+# hide
+building.hide_viewport = True
+DRUtils.get_visibles(building, mode="EDGE")
