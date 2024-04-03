55edb01be0707827a5910d20d9833c79310d126e
Shuyuan Zhang <rayzhang707@gmail.com>
2024-04-03 13:15:33 +0100

    performance and mock inference interface

---
+++ ui_mock_inference.py
@@ -0,0 +1,5 @@
+def inference():
+    print(f"This is a mock function for inference, employed to bypass the torch module which is problematic to run on Windows Blender environment")
+
+def batch_inference():
+    print(f"This is a mock function for batch inference, employed to bypass the torch module which is problematic to run on Windows Blender environment")
