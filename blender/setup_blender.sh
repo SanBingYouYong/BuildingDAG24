792fb97ddcca676ccdb1cdda91cd811365ebcecd
Shuyuan Zhang <rayzhang707@gmail.com>
2024-08-18 14:52:47 +0800

    update

--- blender/setup_blender.sh
+++ blender/setup_blender.sh
@@ -1,4 +1,4 @@
-cd ./blender
+# apt install wget
 wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
 tar -xf blender-3.2.2-linux-x64.tar.xz
 
@@ -7,5 +7,3 @@ tar -czf blender_python_backup.tar.gz -C blender-3.2.2-linux-x64/3.2 python/
 
 # Remove the Python directory after archiving
 rm -rf blender-3.2.2-linux-x64/3.2/python/
-
-cd ../  # Go back to the project root directory
