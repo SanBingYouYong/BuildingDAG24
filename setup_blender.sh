ab7c94b79bc5b6b67ebb049f16fa52d8496387a6
Shuyuan Zhang <rayzhang707@gmail.com>
2024-08-18 14:46:31 +0800

    blender setup script

---
+++ setup_blender.sh
@@ -0,0 +1,11 @@
+cd ./blender
+wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
+tar -xf blender-3.2.2-linux-x64.tar.xz
+
+# Archive Blender's own Python directory
+tar -czf blender_python_backup.tar.gz -C blender-3.2.2-linux-x64/3.2 python/
+
+# Remove the Python directory after archiving
+rm -rf blender-3.2.2-linux-x64/3.2/python/
+
+cd ../  # Go back to the project root directory
